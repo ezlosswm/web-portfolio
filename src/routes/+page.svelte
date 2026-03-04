0d0254438706d2dae15a01942498f6a019b08a0a
ezlosswm <ezlosswm@gmail.com>
2026-03-04 16:35:21 -0600

    added SEO

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,27 @@
 
 <svelte:head>
 	<title>Web Developer Portfolio | Carlos Gomez</title>
+	<meta
+		name="description"
+		content="Portfolio website of Carlos Gomez, a web developer specializing in SvelteKit, Tailwind CSS, Supabase, and modern web technologies. Explore projects, skills, and contact information."
+	/>
+	<meta
+		name="keywords"
+		content="Carlos Gomez, Web Developer, Portfolio, SvelteKit, Tailwind CSS, Supabase, JavaScript, TypeScript, Frontend, Projects, Programmer, Developer, Software Engineer"
+	/>
+	<meta name="author" content="Carlos Gomez" />
+	<meta property="og:title" content="Web Developer Portfolio | Carlos Gomez" />
+	<meta
+		property="og:description"
+		content="Explore the portfolio of Carlos Gomez, web developer with expertise in SvelteKit, Tailwind CSS, and cutting-edge web solutions. View past projects and get in touch."
+	/>
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="carlos-gomez.vercel.app" />
+	<meta name="twitter:title" content="Web Developer Portfolio | Carlos Gomez" />
+	<meta
+		name="twitter:description"
+		content="Discover the work and projects of Carlos Gomez, web developer and UI/UX enthusiast."
+	/>
 </svelte:head>
 
 <main class="px-6 lg:px-12">
