1ad0f234baea66b4c22f0b10ef0a9805a6602de2
ezlosswm <ezlosswm@gmail.com>
2026-06-27 11:56:16 -0600

    added markit

--- src/lib/projectList.ts
+++ src/lib/projectList.ts
@@ -20,7 +20,7 @@ export const projectList: ProjectMetaData[] = [
 				'I built the project using Svelte and Tailwind CSS. I chose Svelte because it compiles components into optimized JavaScript, which helps keep the application lightweight and fast. Tailwind allowed me to quickly design and iterate on the layout while maintaining a consistent visual style. The page includes a simple profile section with an avatar, a short bio, and a list of customizable links that direct users to different platforms or resources. I designed the layout with a mobile-first approach, since most users typically access Link in Bio pages from social media apps on their phones. Since this was a practice project, I kept the scope intentionally small and focused on building something clean and efficient. The project helped me reinforce my understanding of component-based development in Svelte and utility-first styling with Tailwind, while also demonstrating how a simple idea can be turned into a polished and functional interface.'
 		},
 		technicalHighlights: {
-			performance: '90',
+			performance: '95',
 			efficiency: '74'
 		},
 		url: 'https://ezlos.vercel.app'
@@ -46,9 +46,35 @@ export const projectList: ProjectMetaData[] = [
 				'SuggestionsBox solves this by providing a clean, accessible space for anyone to post and view on ideas without any technical hurdles. By allowing community members to upvote suggestions, the platform naturally highlights the most important improvements, while clear status labels keep everyone informed on progress. It replaces chaotic communication channels with a single, transparent, and democratic system that helps organizers focus on what their audience truly wants, all while maintaining the simplicity of a digital suggestion box.'
 		},
 		technicalHighlights: {
-			performance: '99',
+			performance: '98',
 			efficiency: '111'
 		},
 		url: 'https://suggestionsbox.vercel.app/'
+	},
+	{
+		slug: 'markit',
+		name: 'MarkIt',
+		description:
+			'The playful bookmark manager for organized minds. Save links, images, and snippets in one beautiful place.',
+		projectDetails: [
+			{ title: 'Year', description: '2026' },
+			{ title: 'Role', description: 'Frontend Developer' },
+			{ title: 'Tech', description: 'Sveltekit, Convex' }
+		],
+		imageMetaData: {
+			src: './assets/markit.webp',
+			alt: 'MarkIt hero page'
+		},
+		caseStudy: {
+			challenge:
+				"Keep track of valuable links, articles, and online resources, create a clutter of open browser tabs, scattered bookmarks, and links hastily pasted into random chats or note apps. Traditional browser bookmarking tools lack intuitive organization and flexibility, which causes important references to get lost and forgotten. When users try to find a specific link days or weeks later, they're forced to dig through chaotic histories, creating a frustrating bottleneck in daily workflows.",
+			solution:
+				"Keep track of valuable links, articles, and online resources, create a clutter of open browser tabs, scattered bookmarks, and links hastily pasted into random chats or note apps. Traditional browser bookmarking tools lack intuitive organization and flexibility, which causes important references to get lost and forgotten. When users try to find a specific link days or weeks later, they're forced to dig through chaotic histories, creating a frustrating bottleneck in daily workflows."
+		},
+		technicalHighlights: {
+			performance: '97',
+			efficiency: '74'
+		},
+		url: 'https://mark-it-mu.vercel.app/'
 	}
 ];
